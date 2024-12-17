df095266e5fa9e432557b108429d0e4cc56fbc07
Maged Eshak <magedeshak39@gmail.com>
2024-12-17 11:32:28 +0200

    first commit

---
+++ array.js
@@ -0,0 +1,23 @@
+console.log("Array Big Introduction");
+console.log("----------------------");
+
+let myFriends = ["Maged", "Abanob", "Youseef", ["Shery", "Sara"]];
+console.log(myFriends);
+
+
+console.log(myFriends[0]);
+
+myFriends[3][0] = "Mama";
+console.log(myFriends[3]);
+
+myFriends[3] = ["Mama", "Me"];
+console.log(myFriends[3]);
+console.log(myFriends);
+console.log(Array.isArray(myFriends));
+
+console.log("----------------------");
+console.log("Using Length With Array");
+console.log("----------------------");
+
+
+
