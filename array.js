2906b159e312b803b4cf14d530cbaee3ad0b839d
Maged Eshak <magedeshak39@gmail.com>
2024-12-17 13:31:15 +0200

    second commit

--- array.js
+++ array.js
@@ -28,3 +28,25 @@ console.log(myFriend.length-1);
 myFriend[myFriend.length - 1] = ["Ahmed", "Mohammed", "Ebram"];
 console.log(myFriend);
 
+console.log("----------------------");
+console.log("3- Add And Remove From Array");
+console.log("----------------------");
+
+let myFriendse = ["Maged", "Abanob", "Youseef", ["Shery", "Sara"]];
+console.log(myFriendse);
+
+myFriendse.unshift("Mina"); //   - unshift("", "") Add Element To The First
+
+console.log(myFriendse);
+
+myFriendse.push("Nasser"); //   - push("", "") Add Element To The End
+console.log(myFriendse);
+
+let first = myFriendse.shift(); //   - shift() Remove First Element From Array
+console.log(myFriendse);
+console.log(`First Name Is ${first}`);
+
+let last =myFriendse.pop(); //   - pop() Remove Last Element From Array
+console.log(myFriendse);
+console.log(`Last Name Is ${last}`);
+
