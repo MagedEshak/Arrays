c86c147f50bde009ec52f792b976c19fcea9006f
Maged Eshak <magedeshak39@gmail.com>
2024-12-17 15:38:08 +0200

    التكليف الثاني

--- assigmentArray.js
+++ assigmentArray.js
@@ -2,3 +2,21 @@ console.log("----------------------");
 console.log("-- حل التكليفات --");
 console.log("----------------------");
 
+
+console.log("-- التكليف 01 --");
+console.log("----------------------");
+
+let Friends = ["Ahmed", "Elham", "Osama", "Gamal"];
+let num = 3;
+
+// Method 1
+console.log(Friends.slice(false,num)); // ["Ahmed", "Elham", "Osama"];
+
+// Method 2
+let lastIndex = Friends.pop();
+console.log(Friends); // ["Ahmed", "Elham", "Osama"];
+
+console.log("----------------------");
+console.log("-- التكليف 02 --");
+console.log("----------------------");
+
